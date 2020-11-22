8c435f273158a2f20211faac571d8f0b5501ada2
Brian Carducci <briancarducci@gmail.com>
2020-11-22 02:25:21 -0500

    byte array stuff

--- app/src/main/java/Commands.kt
+++ app/src/main/java/Commands.kt
@@ -1,5 +1,15 @@
 enum class Commands {
-    CONNECT,
-    DISCONNECT,
-    SEND
+    PI_CTRL_SYN_ACK,    // Server: You are now connected - continue
+    PI_CTRL_BUSY,       // Server: Someone is already connected - you must disconnect
+
+    PI_CTRL_SYN,        // Client: Request to connect
+    PI_CTRL_HEARTBEAT,  // Client: Send heartbeat so server can disconnect if connection is lost
+    PI_CTRL_DISCONNECT, // Client: Let server know you are disconnecting
+
+    PI_CTRL_SET_NAME,   // Client: Send current name so server can say who is currently connected on a PI_CTRL_BUSY
+    PI_CTRL_MOUSE_MV,   // Client: Send x,y of relative position to move mouse to
+    PI_CTRL_MOUSE_DOWN, // Client: Say to press mouse down (no data required)
+    PI_CTRL_MOUSE_UP,   // Client: Say to press mouse up (no data required)
+    PI_CTRL_MOUSE_CLK,  // Client: Say to click (mouseup, then mousedown) mouse (no data required)
+    PI_CTRL_KEY_PRESS,  // Client: Send UTF-8 value of key to be pressed (details TBD)
 }
--- app/src/main/java/com/example/picontrol/FirstFragment.kt
+++ app/src/main/java/com/example/picontrol/FirstFragment.kt
@@ -10,11 +10,14 @@ import android.widget.Button
 import androidx.navigation.fragment.findNavController
 
 import Commands
+import java.nio.charset.Charset
 import kotlin.reflect.typeOf
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
+val deviceName = android.os.Build.MODEL.toByteArray();
+
 class FirstFragment : Fragment() {
 
     override fun onCreateView(
@@ -29,10 +32,21 @@ class FirstFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         view.findViewById<Button>(R.id.send_button).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-            for (cmd in Commands.values()) {
-                Log.d(cmd.toString(), cmd.ordinal.toString())
-            }
+//            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+//            send(cmd.ordinal, deviceName.length, deviceName)
+
+            Log.d("device name", deviceName.toString(Charsets.UTF_8))
+            val sent = makeMsg(Commands.PI_CTRL_SET_NAME.ordinal, deviceName.size, deviceName)
+            Log.d("command",  sent[0].toInt().toString())
+            Log.d("size",  sent[1].toInt().toString())
+            val jeff = sent[1].toInt()
+            Log.d("payload", (sent.sliceArray(IntRange(2, jeff + 1))).toString(Charsets.UTF_8))
         }
+    }
+
+    private fun makeMsg(cmd: Int, payloadSize: Int, payload: ByteArray): ByteArray {
+//        return byteArrayOf(cmd.toByte(), payloadSize.toByte(), *payload)
+        Log.d("payload size", payloadSize.toString())
+        return byteArrayOf(cmd.toByte(), payloadSize.toByte()) + payload
     }
 }
